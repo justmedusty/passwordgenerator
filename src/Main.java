78cf88104e0597f59680659718d559f9d8adc61d
justmedusty <dustyn_gibb@hotmail.com>
2021-08-07 19:53:23 -0400

    Added a loop to method main, added /bin/ to gitignore
    Signed-off-by: justmedusty <dustyn_gibb@hotmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scan = new Scanner(System.in);
         String password;
+        boolean repeat = false;
+        String yesOrNo;
+
         password = Password.createPassword();
-        System.out.printf("Your generated password is: %n %n %s",password);
+        System.out.printf("Your generated password is: %n %n %s %n", password);
+
+        System.out.println("Do you want to generate another password? (Y/N)");
+        yesOrNo = scan.nextLine();
+        if (yesOrNo.equalsIgnoreCase("y") || yesOrNo.equalsIgnoreCase("yes"))
+            main(args);
+
 
     }
+
+
 }
+
+
